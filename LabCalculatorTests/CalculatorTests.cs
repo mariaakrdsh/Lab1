c4c7725133669b30e25b1aa1fce3cd9ae2a29614
mariaakrdsh <91921062+mariaakrdsh@users.noreply.github.com>
2021-11-25 21:36:44 +0200

    Add Unit Tests aboba

---
+++ LabCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void Power()
+        {
+            string numberr = "7";
+            string power = "0";
+            double expected = 1;
+
+            //act
+
+            double actual = Calculator.Evaluate(numberr + "^" + power);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IncDec()
+        {
+            string numberr = "10";
+            double expected = 11;
+
+            //act
+
+            double actual = Calculator.Evaluate("inc(" + numberr + ")");
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ModDiv()
+        {
+            string numberr = "5";
+            string mod = "0";
+            double expected = double.PositiveInfinity;
+
+            //act
+
+            double actual = Calculator.Evaluate(numberr + "mod" + mod);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
